89b93e738b8591c4b1356a941695fa87c75f062f
Markel Neila <markel.neila.2004@gmail.com>
2025-05-06 09:06:38 +0200

    Nuevos java

--- src/conexion.java
+++ src/conexion.java
@@ -11,105 +11,120 @@ public class conexion{
             //CONEXION A LA BASE DE DATOS
             Connection conn = DriverManager.getConnection(url,user,password);
             System.out.println("Conexion exitosa a la base de datos");
+            //MENU
             System.out.println("Elige una opción:");
-            System.out.println("1. Autores");
-            System.out.println("2. Clientes");
-            System.out.println("3. Editoriales");
-            System.out.println("4. Ejemplares");
-            System.out.println("5. Empleados");
-            System.out.println("6. Libros");
-            System.out.println("7. Prestamos");
-            System.out.println("8. Quejas");
+            System.out.println("1. Clientes");
+            System.out.println("2. Ejemplares");
+            System.out.println("3. Empleados");
             int tabla = scanner.nextInt();
             scanner.nextLine();
             switch (tabla){
                 case 1:
                 System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Autor");
-                System.out.println("2. Actualizar Autor");
-                System.out.println("3. Eliminar Autor");
-                System.out.println("4. Ver Autor");
+                System.out.println("1. Insertar Cliente");
+                System.out.println("2. Actualizar Cliente");
+                System.out.println("3. Eliminar Cliente");
+                System.out.println("4. Ver Clientes");
                 int opcion1 = scanner.nextInt();
                 scanner.nextLine();
                 switch (opcion1){
                     case 1:
-                    System.out.println("Introduce el nombre del autor");
+                    System.out.println("Introduce el nombre del Cliente");
                     String nombre = scanner.next();
                     scanner.nextLine();
-                    System.out.println("Introduce los apellidos del autor");
+                    System.out.println("Introduce los apellidos del Cliente");
                     String apellidos = scanner.next();
                     scanner.nextLine();
-                    String str = "INSERT INTO autores(nombre,apellidos)";
-                    str+= "VALUES ('"+ nombre +"','"+ apellidos +"')";
+                    System.out.println("Introduce el correo del Cliente");
+                    String correo = scanner.next();
+                    scanner.nextLine();
+                    System.out.println("Introduce el telefono del Cliente");
+                    String telefono = scanner.next();
+                    scanner.nextLine();
+                    System.out.println("Introduce la contraseña");
+                    String contraseña = scanner.next();
+                    scanner.nextLine();
+                    String str = "INSERT INTO clientes_1(Nombre,Apellidos,Correo,Telefono,Contraseña)";
+                    str+= "VALUES ('"+ nombre +"','"+ apellidos +"','"+ correo +"','"+ telefono +"','"+ contraseña +"')";
                     Statement stmt=conn.createStatement();
                     stmt.executeUpdate(str);
                     System.out.println("Registro Añadido");
+                    break;
+                    case 2:
+
+                    break;
+                    case 3:
+
+                    break;
+                    case 4:
+
                     break;
                 }
                 break;
                 case 2:
                 System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Cliente");
-                System.out.println("2. Actualizar Cliente");
-                System.out.println("3. Eliminar Cliente");
-                System.out.println("4. Ver Cliente");
-                int opcion2 = scanner.nextInt();
-                scanner.nextLine();
-                break;
-                case 3:
-                System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Editorial");
-                System.out.println("2. Actualizar Editorial");
-                System.out.println("3. Eliminar Editorial");
-                System.out.println("4. Ver Editorial");
-                int opcion3 = scanner.nextInt();
-                scanner.nextLine();
-                break;
-                case 4:
-                System.out.println("Elige una opción:");
                 System.out.println("1. Insertar Ejemplares");
                 System.out.println("2. Actualizar Ejemplares");
                 System.out.println("3. Eliminar Ejemplares");
                 System.out.println("4. Ver Ejemplares");
-                int opcion4 = scanner.nextInt();
+                int opcion2 = scanner.nextInt();
                 scanner.nextLine();
+                switch(opcion2){
+                    case 1:
+                    System.out.println("Introduce el estado del ejemplar");
+                    String estado = scanner.next();
+                    scanner.nextLine();
+                    System.out.println("Introduce el codigo del ejemplar");
+                    String cod_libro = scanner.next();
+                    scanner.nextLine();
+                    String str="INSERT INTO ejemplares";
+                    str+="VALUES('"+estado+"',"+cod_libro+"')";
+                    Statement stmt=conn.createStatement();
+                    stmt.executeUpdate(str);
+                    System.out.println("Ejemplar Añadido");
+                    break;
+                    case 2:
+
+                    break;
+                    case 3:
+
+                    break;
+                    case 4:
+
+                    break;
+                }
                 break;
-                case 5:
+                case 3:
                 System.out.println("Elige una opción:");
                 System.out.println("1. Insertar Empleados");
                 System.out.println("2. Actualizar Empleados");
                 System.out.println("3. Eliminar Empleados");
                 System.out.println("4. Ver Empleados");
-                int opcion5 = scanner.nextInt();
+                int opcion3 = scanner.nextInt();
                 scanner.nextLine();
-                break;
-                case 6:
-                System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Libro");
-                System.out.println("2. Actualizar Libro");
-                System.out.println("3. Eliminar Libro");
-                System.out.println("4. Ver Libros");
-                break;
-                case 7:
-                System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Prestamo");
-                System.out.println("2. Actualizar Prestamo");
-                System.out.println("3. Eliminar Prestamo");
-                System.out.println("4. Ver Prestamos");
-                break;
-                case 8:
-                System.out.println("Elige una opción:");
-                System.out.println("1. Insertar Queja");
-                System.out.println("2. Actualizar Queja");
-                System.out.println("3. Eliminar Queja");
-                System.out.println("4. Ver Quejas");
-                break;
+                switch(opcion3){
+                    case 1:
 
+                    break;
+                    case 2:
+
+                    break;
+                    case 3:
+
+                    break;
+                    case 4:
+
+                    break;
+                }
+                break;
                 default:
                     System.out.println("Opción no válida.");
+                    break;
             }
         }catch(SQLException e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.print("Error en la conexión ");
+
         }
     }
 }
