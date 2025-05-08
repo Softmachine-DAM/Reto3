f6ff20e9b437b87b961a91ce761c7845e5d5bcdb
Ander-Arias <ander.arias01@somo.eus>
2025-05-08 13:46:40 +0200

    Arreglos menus V2

--- src/Menu.java
+++ src/Menu.java
@@ -22,47 +22,75 @@ public class Menu {
                         Cliente.insertClientes();
                     }else if(opcion1==2){
                     
-                }else if(opcion1==3){
+                    }else if(opcion1==3){
 
-                }else{
+                    }else{
 
-                }
-                break;
+                    }
+                    break;
                 case 2:
-                Ejemplares.usoEjemplares();
-                int opcion2 = scanner.nextInt();
-                scanner.nextLine();
-                if(opcion2==1){
-                    Ejemplares.inserEjemplares();
-                }else if(opcion2==2){
+                    Ejemplares.usoEjemplares();
+                    int opcion2 = scanner.nextInt();
+                    scanner.nextLine();
+                    if(opcion2==1){
+                        Ejemplares.inserEjemplares();
+                    }else if(opcion2==2){
 
-                }else if(opcion2==3){
+                    }else if(opcion2==3){
 
-                }else{
+                    }else{
 
-                }
+                    }
                 case 3:
-                Empleados.usoEmpleados();
-                int opcion3 = scanner.nextInt();
-                scanner.nextLine();
-                if(opcion3==1){
-                    Empleados.insertEmpleados();
-                }else if(opcion3==2){
+                    Empleados.usoEmpleados();
+                    int opcion3 = scanner.nextInt();
+                    scanner.nextLine();
+                    if(opcion3==1){
+                        Empleados.insertEmpleados();
+                    }else if(opcion3==2){
 
-                }else if(opcion3==3){
+                    }else if(opcion3==3){
 
-                }else{
+                    }else{
 
-                }
-                break;
+                    }
+                    break;
+                case 4:
+                    break;
                 default:
                     System.out.println("Opción no válida.");
                     break;
             }
-        }catch(SQLException e){
-            //e.printStackTrace();
-            System.out.print("Error en la conexión ");
-
-        }
+        } while (opcionME != 4);
     }
+    public static void menuClientes(){
+        Scanner scanner = new Scanner(System.in);
+        int opcionMC = 0;
+        do {
+            System.out.println("Elige una opción:");
+            System.out.println("1. Prestamos pendientes");
+            System.out.println("2. Libros disponibles");
+            System.out.println("3. Prestar libro");
+            System.out.println("4. Cerrar sesion");
+            opcionMC = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcionMC){
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        } while (opcionMC != 4);
+    }
+}
 
