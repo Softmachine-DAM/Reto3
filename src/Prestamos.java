fa9e5c2f252683e276fdbb77aaab9b4808b62d39
Ander-Arias <ander.arias01@somo.eus>
2025-05-12 09:34:27 +0200

    Prestamos pendientes

---
+++ src/Prestamos.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Prestamos {
+    public static void PrestamosPendientes(SesionActiva sesion){
+        String url="jdbc:mysql://127.0.0.1:3306/biblioteca";
+        String user="Grupo3";
+        String password="Reto3";
+        try {
+            Connection conn = DriverManager.getConnection(url,user,password);
+            String slctPrest = "SELECT FROM prestamos WHERE ID = ? AND devuelto = 0";
+            try (PreparedStatement stmt = conn.prepareStatement(slctPrest)){
+                stmt.setInt(1, sesion.getId());
+                ResultSet rsPrest = stmt.executeQuery();
+                if (rsPrest.next()) {
+                    System.out.println("Tienes pendiente de devolver el libro" + ObtenerTituloLibro(rsPrest.getInt("id_libro")));
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al consultar prestamos pendientes");
+            }
+        } catch (Exception e) {
+            
+        }
+    }
+    public static void PrestarLibro(SesionActiva sesion){
+
+    }
+    public static void VerLibros(SesionActiva sesion){
+
+    }
+    public static void DevolverLibro(SesionActiva sesion){
+
+    }
+    public static String ObtenerTituloLibro(int idLibro){
+        String slctLibro = "SELECT titulo from Libros WHERE id_libro = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(slctLibro)) {
+            stmt.setInt(1, idLibro);
+            ResultSet rsLibro = stmt.executeQuery();
+            if (rsLibro.next()) {
+                return rsLibro.getString("Nombre");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return "Libro desconocido";
+    }
+}
